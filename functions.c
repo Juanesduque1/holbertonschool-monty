1f03a15d2761bca719f043cddc5135a07af7fe8d
CharlineMosquera <char951144@gmail.com>
2023-01-04 14:01:58 -0500

    push function

--- functions.c
+++ functions.c
@@ -1,12 +1,10 @@
 #include "monty.h"
 
 /**
-*main - Main function
-*@argc: Quantity of arguments that enter the function
-*@argv: Arguments
-*Return: Always Zero
-*/
-
+ * f_pall - prints all the values on the stack
+ * @stack: stack
+ * @line_number: line where the instruction is located
+ */
 void f_pall(sstack_t **stack, unsigned int line_number)
 {
 	sstack_t *aux;
@@ -23,10 +21,25 @@ void f_pall(sstack_t **stack, unsigned int line_number)
 		printf("%d\n", aux->n);
 		aux = aux->next;
 	}
-
 }
 
+/**
+ * f_push - pushes an element to the stack
+ * @stack: stack
+ * @line_number: line where the instruction is located
+ */
 void f_push(sstack_t **stack, unsigned int line_number)
 {
+	sstack_t *new;
+	char integer;
 
+	new = (sstack_t *)malloc(sizeof(sstack_t));
+	if (!new)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = atoi(integer);
+	new->prev = NULL;
+	new->next = *stack;
 }
